b6ad3b65f21a8efee82551315d12241b65a59299
Arne Munthe-Kaas <arnegiacomo@gmail.com>
2023-02-06 14:09:48 +0100

    .

--- app/src/main/java/no/hvl/dat153/quizapp/activites/DatabaseActivity.java
+++ app/src/main/java/no/hvl/dat153/quizapp/activites/DatabaseActivity.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 
 import no.hvl.dat153.quizapp.R;
 import no.hvl.dat153.quizapp.db.AnimalDAO;
+import no.hvl.dat153.quizapp.util.Util;
 import no.hvl.dat153.quizapp.view.AnimalAdapter;
 
 public class DatabaseActivity extends AppCompatActivity {
@@ -35,35 +36,15 @@ public class DatabaseActivity extends AppCompatActivity {
         deletebtn.setOnClickListener(view -> animalAdapter.updateAdapter());
 
         View addbtn = findViewById(R.id.addbtn);
-        addbtn.setOnClickListener(view -> pickImageFromGallery());
+        addbtn.setOnClickListener(view -> Util.startActivity(this, AddEntryActivity.class));
 
         animalAdapter.notifyDataSetChanged();
     }
 
-
-    private void pickImageFromGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
-    }
-
-    private static final int REQUEST_CODE_PICK_IMAGE = 1;
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK) {
-            Uri imageUri = data.getData();
-
-            Bitmap bitmap = null;
-            try {
-                InputStream inputStream = getContentResolver().openInputStream(imageUri);
-                bitmap = BitmapFactory.decodeStream(inputStream);
-            } catch (FileNotFoundException e) {
-                // Handle the error
-            }
-            AnimalDAO.get().addAnimal("Test", bitmap);
-            animalAdapter.notifyDataSetChanged();
-        }
+    protected void onResume() {
+        super.onResume();
+        animalAdapter.notifyDataSetChanged();
     }
+
 }
